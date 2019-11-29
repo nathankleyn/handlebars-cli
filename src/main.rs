5b852d1b9aa09abf52a02b2fb8c8081a876705ee
Nathan Kleyn <nathan@nathankleyn.com>
2019-11-29 11:44:37 +0000

    Add a complete CLI tool for templating with Handlebars.
    Given a set of JSON properties, and a Handlebars template, this CLI tool
    will return the result of trying to template thise properties into the
    template using Handlebars.
    
    You can install this tool with `cargo install`.
    
    A comprehensive suite of integration tests is provided.

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::env;
+use std::path::PathBuf;
+use std::process;
+use std::str::FromStr;
+
+use handlebars::Handlebars;
+use serde_json::value::Value as Json;
+use snafu::{ResultExt, Snafu};
+
+/// An enumeration of the possible errors this program may encounter.
+#[derive(Debug, Snafu)]
+enum Error {
+    /// This error represents the properties being parsed as invalid JSON.
+    #[snafu(display("Unable to parse properties JSON: {}", source))]
+    PropsInvalidJson { source: serde_json::Error },
+
+    /// This error represents the Handlebars template not being found at the provided path.
+    #[snafu(display("Unable to read template from '{}'.", path))]
+    TemplateNotFound { path: String },
+
+    /// This error represents the Handlebars template not being valid Handlebars syntax.
+    #[snafu(display("File at '{}' was not a valid handlebars template: {}", path.display(), source))]
+    TemplateInvalid { source: handlebars::TemplateFileError, path: PathBuf },
+
+    /// This error represents the Handlebars template attempting to use properties not provided,
+    /// and so rendering failed.
+    #[snafu(display("Template at '{}' failed to render: {}", path.display(), source))]
+    TemplateRenderFailed { source: handlebars::RenderError, path: PathBuf },
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn main () -> () {
+    let mut args = env::args();
+    args.next(); // skip own filename
+
+    let (raw_props, raw_filename) = match (args.next(), args.next()) {
+        (Some(raw_props), Some(raw_filename)) => (raw_props, raw_filename),
+        _ => {
+            eprintln!("Usage: ./handlebars-cli '{{\"json\": \"data\"}}' template.hbs");
+            process::exit(1);
+        },
+    };
+
+    match execute_handlebars_templating(raw_props, raw_filename) {
+        Ok(data) => {
+            println!("{}", data)
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1)
+        }
+    }
+}
+
+/// Given a string which should contain valid JSON representing a set of properties, take those
+/// properties and interpolate them into a handlebars template at the given path.
+///
+/// If everything succeeds, this will return the templated result.
+///
+/// It fails if the properties are not valid JSON.
+/// It fails if the template file could not be found.
+/// It fails if the template file is not a valid Handlebars template.
+/// It fails if the template file used properties that were not available.
+fn execute_handlebars_templating(raw_props: String, raw_filename: String) -> Result<String, Error> {
+    let props = Json::from_str(&raw_props)
+        .context(PropsInvalidJson {})?;
+
+    let filename = PathBuf::from(&raw_filename);
+    if !filename.exists() { return TemplateNotFound { path: raw_filename }.fail() }
+
+    let mut handlebars = Handlebars::new();
+    handlebars.set_strict_mode(true);
+
+    handlebars.register_template_file(&raw_filename, &filename)
+        .context(TemplateInvalid { path: &filename })?;
+
+    handlebars.render(&raw_filename, &props)
+        .context(TemplateRenderFailed { path: &filename })
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,88 @@
+use assert_cmd::prelude::*;
+use std::io::Write;
+use std::process::Command;
+use tempfile::NamedTempFile;
+
+// These tests have to be integration tests as otherwise they cannnot use the
+// built binary.
+
+macro_rules! when_binary_run {
+    () => {
+        Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
+    };
+}
+
+#[test]
+fn test_fails_with_usage_if_no_params() {
+    when_binary_run!()
+        .assert()
+        .failure()
+        .stderr("Usage: ./handlebars-cli '{\"json\": \"data\"}' template.hbs\n");
+}
+
+#[test]
+fn test_fails_if_only_one_param() {
+    when_binary_run!()
+        .arg("{}")
+        .assert()
+        .failure()
+        .stderr("Usage: ./handlebars-cli '{\"json\": \"data\"}' template.hbs\n");
+}
+
+#[test]
+fn test_fails_if_properties_are_not_valid_json() {
+    when_binary_run!()
+        .arg("^_^")
+        .arg("/tmp/foo")
+        .assert()
+        .failure()
+        .stderr("Unable to parse properties JSON: expected value at line 1 column 1\n");
+}
+
+#[test]
+fn test_fails_if_template_file_not_found() {
+    when_binary_run!()
+        .arg("{}")
+        .arg("/tmp/foo")
+        .assert()
+        .failure()
+        .stderr("Unable to read template from '/tmp/foo'.\n");
+}
+
+#[test]
+fn test_fails_if_template_file_uses_property_not_found() {
+    when_binary_run!()
+        .arg("{}")
+        .arg("/tmp/foo")
+        .assert()
+        .failure()
+        .stderr("Unable to read template from '/tmp/foo'.\n");
+}
+
+#[test]
+fn test_succeeds_when_noop() {
+    let mut file = NamedTempFile::new().unwrap();
+    writeln!(file, "Hello World!").unwrap();
+
+    when_binary_run!()
+        .arg("{}")
+        .arg(file.path())
+        .assert()
+        .success()
+        .stdout("Hello World!\n\n");
+}
+
+
+#[test]
+fn test_succeeds_when_templating_successful() {
+    let mut file = NamedTempFile::new().unwrap();
+    // Note that we have to escape the brackets.
+    writeln!(file, "Hello {{{{name.first}}}} {{{{name.last}}}}!").unwrap();
+
+    when_binary_run!()
+        .arg("{ \"name\": { \"first\": \"Foo\", \"last\": \"Bar\" }}")
+        .arg(file.path())
+        .assert()
+        .success()
+        .stdout("Hello Foo Bar!\n\n");
+}
